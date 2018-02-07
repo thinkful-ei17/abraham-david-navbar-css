ce0b83f757a48024b2eda8ea9522c1e57b91e4c2
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-02-07 12:51:51 -0800

    Add text-decoration and class toggle for menu items

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-'use strict';
+
 
 $(() =>{
   $('.top-nav').on('click', '.hamburger', ()=>{
     $('nav.main-nav').toggleClass('hidden');
   });
   $('nav ul li a').on('click', e => {
+    $(e.currentTarget).toggleClass('active');
     const current = $(e.currentTarget).next('nav');
     $('nav nav').not(current).addClass('hidden');
+    $('nav ul li a').not($(e.currentTarget)).removeClass('active');
     current.toggleClass('hidden');
   });
   
