066e2194c48c77b9d0c1a68e1af2ad29016bf728
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-02-07 13:08:17 -0800

    Update styles for sub-menu

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ $(() =>{
     $('nav.main-nav').toggleClass('hidden');
   });
   $('nav ul li a').on('click', e => {
-    $(e.currentTarget).toggleClass('active');
+    
     const current = $(e.currentTarget).next('nav');
     $('nav nav').not(current).addClass('hidden');
-    $('nav ul li a').not($(e.currentTarget)).removeClass('active');
+    
     current.toggleClass('hidden');
   });
   
