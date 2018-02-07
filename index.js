f7ee801e72c3ca39d1df581e5c9f4f145f034bfe
David Park <dysp92@gmail.com>
2018-02-07 09:48:24 -0800

    added jquery handler for nav links

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+'use strict';
+
 $(() =>{
   $('.top-nav').on('click', '.hamburger', ()=>{
     $('nav.main-nav').toggleClass('hidden');
   });
+  $('nav ul li a').on('click', e => {
+    $(e.currentTarget).next('nav').toggleClass('hidden');
+  });
+  
 });
